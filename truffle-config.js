624a54ecfa24d35040cb534b41e5ba366f1afbdc
josealonso <jose.alonso.programmer@gmail.com>
2021-08-22 23:53:23 +0200

    Deployed the contract succesfully on the ganache local node.

--- truffle-config.js
+++ truffle-config.js
@@ -1,40 +1,30 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",    // 1337
+    }    // ,
+    // ganache_local: {
+    //   provider: function () {
+    //     return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex);
+    //   },
+    //   network_id: 1337
+    // }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0"
+    }
+  }
+};
+
+
+
+//  networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -71,17 +61,17 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+//  },
 
   // Set default mocha options here, use special reporters etc.
-  mocha: {
+//  mocha: {
     // timeout: 100000
-  },
+//  },
 
   // Configure your compilers
-  compilers: {
-    solc: {
-       version: "0.8.0",    
+ // compilers: {
+   // solc: {
+     //  version: "0.8.0",    
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -90,8 +80,8 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
-  },
+  //  }
+//  },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
   //
@@ -99,8 +89,8 @@ module.exports = {
   // those previously migrated contracts available in the .db directory, you will need to run the following:
   // $ truffle migrate --reset --compile-all
 
-  db: {
-    enabled: false
-  }
-};
+//  db: {
+  //  enabled: false
+//  }
+//};
 
