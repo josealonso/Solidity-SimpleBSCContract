b4f8d9389db2de9d2c802c70288f7e102c314b3b
josealonso <jose.alonso.programmer@gmail.com>
2021-08-23 12:17:15 +0200

    Fixed a naming issue with the Test7Token.json file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Test7Token from "./contracts/Test7.json";
+import Test7Token from "./contracts/Test7Token.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded: false, kycAddress: "0x123", tokenSaleAddress: null, userTokens: 0 };
 
   componentDidMount = async () => {
     try {
@@ -22,10 +22,10 @@ class App extends Component {
         Test7Token.networks[this.networkId] && Test7Token.networks[this.networkId].address,
       );
 
-/*      this.myTokenSale = new this.web3.eth.Contract(
-        MyTokenSale.abi,
-        MyTokenSale.networks[this.networkId] && MyTokenSale.networks[this.networkId].address,
-      ); */
+      /*      this.myTokenSale = new this.web3.eth.Contract(
+              MyTokenSale.abi,
+              MyTokenSale.networks[this.networkId] && MyTokenSale.networks[this.networkId].address,
+            ); */
 
       console.log("---------------- this.networkId: " + this.networkId);
       console.log("Test7Token.networks: " + Test7Token.networks);
@@ -33,8 +33,8 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-//      this.listenToTokenTransfer();
-//      this.setState({ loaded: true, tokenSaleAddress: Test7Token.networks[this.networkId].address }, this.updateUserTokens);  
+           this.listenToTokenTransfer();
+           this.setState({ loaded: true, tokenSaleAddress: Test7Token.networks[this.networkId].address }, this.updateUserTokens);  
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -43,30 +43,58 @@ class App extends Component {
       console.error(error);
     }
   };
-  
-  runExample = async () => {
-    const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    // let myBalance = await contract.methods.balanceOf({ from: accounts[0] });
-    let userTokens = await this.myTest7Token.methods.balanceOf(this.accounts[0]).call(); 
+  updateUserTokens = async () => {
+    // call() is a reading, gas-free operation
+    let userTokens = await this.myToken.methods.balanceOf(this.accounts[0]).call();
     console.log("userTokens: " + userTokens);
-    // Get the value from the contract to prove it worked.
-//    const response = await contract.methods.get().call();
+    this.setState({ userTokens: userTokens });
+  }
 
-  };
+  listenToTokenTransfer = async () => {
+    this.myTest7Token.events.Transfer({ to: this.accounts[0] }).on("data", this.updateUserTokens);
+  }
+
+  handleBuyTokens = async () => {
+    await this.myTokenSale.methods.buyTokens(this.accounts[0]).send({ from: this.accounts[0], value: "1" });
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
+
+  // Stores a given value, 5 by default.
+  // let myBalance = await contract.methods.balanceOf({ from: accounts[0] });
+  // let userTokens = await this.myTest7Token.methods.balanceOf(this.accounts[0]).call(); 
+  // Get the value from the contract to prove it worked.
+  //    const response = await contract.methods.get().call();
+  // };
 
   render() {
-/*    if (!this.state.web3) {
+    if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
-    } */
+    }
     return (
       <div className="App">
         <h1>Balance on BSC</h1>
-        <p>
-          You currently have:<strong><p> {this.state.userTokens} TST7 tokens</p></strong>.
-        </p>
+
+        <h2>Enable your account</h2>
+        {/* Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
+        <button type="button" onClick={this.handleKycWhitelisting}>Add Address to Whitelist</button>
+        <h2>Buy Complu-Tokens</h2> */}
+        <p>Send Ether to this address: {this.state.tokenSaleAddress}</p>
+        <p>You currently have: {this.state.userTokens} Test7 tokens</p>
+        <button type="button" onClick={this.handleBuyTokens}>Buy Test7 tokens</button>
       </div>
+
     );
   }
 }
